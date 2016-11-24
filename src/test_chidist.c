e6e5eb087ff84f6eb70767576453eeb024555a96
antagon <antagon@codeward.org>
2016-11-24 21:23:35 +0100

    Optimize the char counting algorithm.

--- src/test_chidist.c
+++ src/test_chidist.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <math.h>
+#include <string.h>
 
 #if 0
 bool X2( const std::string& file_path, const std::string& mode )
@@ -82,6 +83,7 @@ bool X2( const std::string& file_path, const std::string& mode )
 }
 #endif
 
+#if 0
 static size_t
 count_char (const char *buff, size_t buff_len, char needle)
 {
@@ -97,10 +99,28 @@ count_char (const char *buff, size_t buff_len, char needle)
 
 	return cnt;
 }
+#endif
+
+static int*
+map_char_counts (const char *buff, size_t buff_len, int array_cnt[256])
+{
+	size_t i;
+	unsigned char byte_val;
+
+	memset (array_cnt, 0, 256);
+
+	for ( i = 0; i < buff_len; i++ ){
+		byte_val = buff[i];
+
+		array_cnt[byte_val] += 1;
+	}
+
+	return array_cnt;
+}
 
 // -----------------------
 
-#define X2_SAMPLE_LEN (1024 * 13)
+#define X2_SAMPLE_LEN (1024 * 32)
 #define X2_POSSIBILITIES 256
 #define X2_FREQ_MIN 163.0
 #define X2_FREQ_MAX 373.0
@@ -109,16 +129,32 @@ int
 testchidist_x2 (const char *file_path)
 {
 	FILE *file;
+	size_t file_len;
 	char buff[X2_SAMPLE_LEN];
 	size_t buff_len;
 	double expected_freq;
 	double chi;
-	int i, char_freq;
+	int char_map[X2_POSSIBILITIES];
+	int i;
 
 	file = fopen (file_path, "rb");
 
 	if ( file == NULL )
 		return -1;
+
+	if ( fseek (file, 0, SEEK_END) == -1 ){
+		fclose (file);
+		return -1;
+	}
+
+	file_len = ftell (file);
+
+	if ( file_len == -1 ){
+		fclose (file);
+		return -1;
+	}
+
+	rewind (file);
 
 	buff_len = fread (buff, 1, sizeof (buff), file);
 
@@ -129,16 +165,31 @@ testchidist_x2 (const char *file_path)
 
 	fclose (file);
 
-	chi = 0.0;
-	expected_freq = X2_SAMPLE_LEN / X2_POSSIBILITIES;
-
-	for ( i = 0; i < X2_POSSIBILITIES; i++ ){
-		char_freq = count_char (buff, buff_len, i);
-		chi += pow (((char_freq * 1.0) - expected_freq), 2) / expected_freq;
+	if ( buff_len < X2_POSSIBILITIES ){
+#if 0
+		fprintf (stderr, "\e[31m%s :: too small %zu\e[0m\n", file_path, file_len);
+#endif
+		return 0;
 	}
 
-	if ( chi < X2_FREQ_MIN || chi > X2_FREQ_MAX )
+	expected_freq = buff_len / X2_POSSIBILITIES;
+	chi = 0.0;
+
+	map_char_counts (buff, buff_len, char_map);
+
+	for ( i = 0; i < X2_POSSIBILITIES; i++ )
+		chi += pow (((char_map[i] * 1.0) - expected_freq), 2) / expected_freq;
+
+	if ( chi < X2_FREQ_MIN || chi > X2_FREQ_MAX ){
+#if 0
+		fprintf (stderr, "\e[31m%s :: len: %zu, chi: %lf\e[0m\n", file_path, buff_len, chi);
+#endif
 		return 0;
+	}
+
+#if 0
+	fprintf (stderr, "\e[92m%s :: len: %zu, chi: %lf\e[0m\n", file_path, buff_len, chi);
+#endif
 
 	return 1;
 }
