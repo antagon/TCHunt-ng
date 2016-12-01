59bdcfeb045874624861f90495554507c00e2667
antagon <antagon@codeward.org>
2016-12-01 21:17:26 +0100

    Derive index into a C array from a data length.

--- src/test/chi_square_model_get_closest.lua
+++ src/test/chi_square_model_get_closest.lua
@@ -8,7 +8,36 @@ function derive_closest (len)
 	return math.pow (2, round (math.log (len) / math.log (2)))
 end
 
-local data_len = math.random (math.pow (2, 5), math.pow (2, 15))
+function derive_cindex (num)
+	-- Powers of 2
+	local min = 5
+	local max = 15
+	local exp = math.log (num) / math.log (2)
 
-print (("Data length: %d\nClosest to: %d"):format (data_len, derive_closest (data_len)))
+	if exp < min or exp > max then
+		return -1
+	end
+
+	return exp - min
+end
+
+local model = {
+	{ len = math.pow (2, 5), min = nil, max = nil },
+	{ len = math.pow (2, 6), min = nil, max = nil },
+	{ len = math.pow (2, 7), min = nil, max = nil },
+	{ len = math.pow (2, 8), min = nil, max = nil },
+	{ len = math.pow (2, 9), min = nil, max = nil },
+	{ len = math.pow (2, 10), min = nil, max = nil },
+	{ len = math.pow (2, 11), min = nil, max = nil },
+	{ len = math.pow (2, 12), min = nil, max = nil },
+	{ len = math.pow (2, 13), min = nil, max = nil },
+	{ len = math.pow (2, 14), min = nil, max = nil },
+	{ len = math.pow (2, 15), min = nil, max = nil }
+}
+
+local data_len = math.random (math.pow (2, 5), math.pow (2, 15))
+local closest = derive_closest (data_len)
+local index = derive_cindex (closest)
+
+print (("Data length: %d\nClosest to: %d\nArray index: %d"):format (data_len, closest, index))
 
