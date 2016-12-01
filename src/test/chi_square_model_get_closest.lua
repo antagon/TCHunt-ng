c082bef7afc2df974eaa033a1bc79f12fd40ab70
antagon <antagon@codeward.org>
2016-12-01 21:27:17 +0100

    Print out model data, fix table to index from zero.

--- src/test/chi_square_model_get_closest.lua
+++ src/test/chi_square_model_get_closest.lua
@@ -22,22 +22,23 @@ function derive_cindex (num)
 end
 
 local model = {
-	{ len = math.pow (2, 5), min = nil, max = nil },
-	{ len = math.pow (2, 6), min = nil, max = nil },
-	{ len = math.pow (2, 7), min = nil, max = nil },
-	{ len = math.pow (2, 8), min = nil, max = nil },
-	{ len = math.pow (2, 9), min = nil, max = nil },
-	{ len = math.pow (2, 10), min = nil, max = nil },
-	{ len = math.pow (2, 11), min = nil, max = nil },
-	{ len = math.pow (2, 12), min = nil, max = nil },
-	{ len = math.pow (2, 13), min = nil, max = nil },
-	{ len = math.pow (2, 14), min = nil, max = nil },
-	{ len = math.pow (2, 15), min = nil, max = nil }
+	[0] = { len = math.pow (2, 5), min = 0.0, max = 0.0 },
+	[1] = { len = math.pow (2, 6), min = 0.0, max = 0.0 },
+	[2] = { len = math.pow (2, 7), min = 0.0, max = 0.0 },
+	[3] = { len = math.pow (2, 8), min = 0.0, max = 0.0 },
+	[4] = { len = math.pow (2, 9), min = 0.0, max = 0.0 },
+	[5] = { len = math.pow (2, 10), min = 0.0, max = 0.0 },
+	[6] = { len = math.pow (2, 11), min = 0.0, max = 0.0 },
+	[7] = { len = math.pow (2, 12), min = 0.0, max = 0.0 },
+	[8] = { len = math.pow (2, 13), min = 0.0, max = 0.0 },
+	[9] = { len = math.pow (2, 14), min = 0.0, max = 0.0 },
+	[10] = { len = math.pow (2, 15), min = 0.0, max = 0.0 }
 }
 
+math.randomseed (os.time ())
 local data_len = math.random (math.pow (2, 5), math.pow (2, 15))
 local closest = derive_closest (data_len)
 local index = derive_cindex (closest)
 
-print (("Data length: %d\nClosest to: %d\nArray index: %d"):format (data_len, closest, index))
+print (("Data length: %d\nClosest to: %d\nArray index: %d => { %d, %f, %f }"):format (data_len, closest, index, model[index].len, model[index].min, model[index].max))
 
