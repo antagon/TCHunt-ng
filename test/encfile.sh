e1d467632776b9328b68f87adfc7ab485bcc9d7e
Dan Antagon <antagon@codeward.org>
2017-02-06 21:45:10 +0100

    Wrapper around bcrypt.

--- test/encfile.sh
+++ test/encfile.sh
@@ -4,16 +4,33 @@ function encfile_bcrypt ()
 	ofile="$2"
 	nocompress="$3"
 
-	execprog bcrypt -r -o "$ifile" > "$ofile"
+	echo -ne "password123\npassword123\n" | execprog bcrypt -r -o $(test $nocompress && echo "-c") -s 4 "$ifile" > "$ofile"
 }
 
 function encfile_openssl ()
 {
-
+	return 1
 }
 
 function encfile_ccrypt ()
 {
+	return 1
+}
 
+function encfile ()
+{
+	memo="$1"
+	which="$2"
+
+	echo -ne "$memo"
+
+	eval "encfile_$which" ${@:3}
+
+	if [ $? -ne 0 ]; then
+		echo -e " [FAIL]"
+		exit 1
+	fi
+
+	echo -e " [OK]"
 }
 
